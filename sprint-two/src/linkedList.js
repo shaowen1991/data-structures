e475572874d728b6de501929b9191bf893cbb048
Hack Reactor Student <student@hackreactor.com>
2017-03-30 13:43:31 -0700

    Implement Linked List Sprint

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;  
+    }
   };
 
   list.removeHead = function() {
+    var formerHead = list.head.value;
+    list.head = list.head.next;
+    return formerHead;
   };
 
   list.contains = function(target) {
+    var searchPtr = list.head;
+    while(searchPtr !== null) {
+      if (searchPtr.value === target) {
+        return true;
+      }
+      searchPtr = searchPtr.next;
+    }
+    return false;
   };
 
   return list;
@@ -25,5 +44,10 @@ var Node = function(value) {
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? 
+ * 
+ * addToTail - O(1)
+ * removeHead - O(1)
+ * contains - O(n)
+ *
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,39 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newTreeNode = Tree(value);
+  this.children.push(newTreeNode);
 };
 
 treeMethods.contains = function(target) {
+  //-- Check senior most node value if equal to target
+  var startNode = this;
+
+  var containsValue = startNode.value === target;
+  //--traverse tree	
+  if(containsValue) {
+  	return true;
+  } else {
+  	if(startNode.children.length === 0) {
+  	  return false;
+  	} else {
+      //var boolean = false;
+      for (var i = 0; i < startNode.children.length; i++) {
+      	var child = startNode.children[i];
+      	containsValue = containsValue || child.contains(target);
+      }
+      return containsValue;
+  	}
+  }
+  
 };
 
 
