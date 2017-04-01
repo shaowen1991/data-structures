60b8a3b15953c9f049c625b502984c144188d92d
Hack Reactor Student <student@hackreactor.com>
2017-03-31 21:28:41 -0700

    Implement Binary Search Tree Data Structure using Objects

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,56 @@
 var BinarySearchTree = function(value) {
-};
+  var binaryTreeNode = {};
+  
+  binaryTreeNode.value = value;
+  binaryTreeNode.left = null;
+  binaryTreeNode.right = null;
 
+  binaryTreeNode.insert = function(value) {
+    if (value < binaryTreeNode.value) {
+      if (binaryTreeNode.left === null) {
+        binaryTreeNode.left = BinarySearchTree(value);        
+      } else {
+        binaryTreeNode.left.insert(value);
+      }
+    } else {
+      if (binaryTreeNode.right === null) {
+        binaryTreeNode.right = BinarySearchTree(value);        
+      } else {
+        binaryTreeNode.right.insert(value);
+      }
+    }
+  };
+
+  binaryTreeNode.contains = function(value) {
+    var containsValue = binaryTreeNode.value === value;
+    
+    if (containsValue) {
+      return true;
+    } else if (value < binaryTreeNode.value) {
+      containsValue = containsValue || binaryTreeNode.left ? binaryTreeNode.left.contains(value) : false;
+    } else {
+      containsValue = containsValue || binaryTreeNode.right ? binaryTreeNode.right.contains(value) : false;
+    }
+
+    return containsValue;
+  };
+
+  binaryTreeNode.depthFirstLog = function(cb) {
+    if (binaryTreeNode !== null) {
+      cb(binaryTreeNode.value);
+      binaryTreeNode.left ? binaryTreeNode.left.depthFirstLog(cb) : null;
+      binaryTreeNode.right ? binaryTreeNode.right.depthFirstLog(cb) : null;
+    }
+  };
+
+  return binaryTreeNode;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
+ * insert: O(log(n))
+ * contains: O(log(n))
+ * depthFirstLog: O(n)
+ *
  */
