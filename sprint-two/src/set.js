47395ac1b0e9a0472460a0af2bc2c10d089c54b4
Hack Reactor Student <student@hackreactor.com>
2017-03-31 20:03:07 -0700

    Implement a Set Data Structure Using a Hash Table

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,21 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = new HashTable(); // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.insert(item, item);
 };
 
 setPrototype.contains = function(item) {
+  return !!this._storage.retrieve(item);
 };
 
 setPrototype.remove = function(item) {
+  this._storage.remove(item);
 };
 
 /*
